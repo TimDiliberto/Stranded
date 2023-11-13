d71f6aaea2eebc273149dfcd7cf27730643bc331
Tim Diliberto <timothydiliberto19@gmail.com>
2023-11-12 23:09:18 -0800

    Setup proper block comment and created pseudo-structure of main with inline comments

--- main.cpp
+++ main.cpp
@@ -1,27 +1,37 @@
 /*
-Stranded_2.0/main.cpp
-Main program flow which controls four main systems:
- * Loader
- * Game
- * Map
- * Screens
+    Stranded_2.0/main.cpp
+    Main program flow which controls four main systems:
+    * Loader
+    * Game
+    * Map
+    * Screens
 
-I referenced JeffM2501's 'RPG Example' to learn raylib and additional C++. His
-project's github is linked below:
-https://github.com/raylib-extras/RPGExample/tree/main
+    I referenced JeffM2501's 'RPG Example' to learn raylib and additional C++. His
+    project's github is linked below:
+    https://github.com/raylib-extras/RPGExample/tree/main
 
-Tim Diliberto
+    @author Tim Diliberto
+    @version 0.0.1.0 11/12/2023
 */
+
+// Include libraries and header files
 #include "raylib.h"
 
+// game states, screens, and setup
+
+// Controls the program
 int main()
 {
     // setup window
     InitWindow(1280, 700, "Stranded 2.0");
     SetTargetFPS(144);
 
+    // load assets
+
     // game loop
     while (!WindowShouldClose()) {
+        // call update for current game state
+
         //update screen for current frame
         BeginDrawing();
         ClearBackground(BLACK);
@@ -30,11 +40,13 @@ int main()
 
         // draw screen
 
-        // audio
+        // update audio
 
         EndDrawing();
     }
 
+    // shutdown audio
+    // cleanup resources
     CloseWindow();
 
     return 0;
